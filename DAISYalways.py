b7784e3232e9d7aded7758a3ffa08a637aef9864
jessicakahn1100 <147117954+jessicakahn1100@users.noreply.github.com>
2026-05-13 20:06:51 -0500

    Add event handling to DAISYalways.py
    Implement event addition to calendar based on input JSON.

---
+++ DAISYalways.py
@@ -0,0 +1,154 @@
+# for adding events to initial calendar
+import os
+import json
+from datetime import datetime
+from datetime import timedelta
+import pickle
+import time
+import traceback
+
+from DAISYhelpers import get_events
+#from DAISYhelpers import format_event
+from DAISYhelpers import check_if_exists
+#from DAISYhelpers import ask_GPT
+from DAISYhelpers import flag_best
+from DAISYhelpers import check_relevance
+
+with open('inpsdict.json', "r") as json_file:
+    full_inputs_dict = json.load(json_file)
+w = []
+for n in full_inputs_dict.keys():
+    inp_dict = full_inputs_dict[n]
+    try:
+        if inp_dict['new'] == "yes":
+            w.append(n) # = n
+            print("will do "+str(n))
+        else:
+            print("not do "+str(n))
+    except Exception as e:
+        print(e)
+        print("not do "+str(n))
+
+
+outs = []
+
+print(w)
+#if len(w) == 0:
+#    w += "5"
+if len(w) > 0:
+    #w += "5"
+    outs.append("NOW RUNNING DAISY ALWAYS")
+    outs.append("NOW RUNNING DAISY ALWAYS")
+    outs.append("NOW RUNNING DAISY ALWAYS")
+    with open("alwaysresult.txt",'w') as file:
+            file.write(str(outs))
+    here = os.getcwd()
+    client_secret_path = here+r'\client_secret.json'
+
+    try:
+        #print('got here 5')
+        with open('service_definition.pkl', 'rb') as f:
+            service = pickle.load(f)
+    except Exception as a:
+        outs.append('got here by failing to load service definition')
+        outs.append(a)
+
+    for n in w:
+        full_inputs_dict[n]['timestamp'] = datetime.now().isoformat()
+        with open('inpsdict.json', 'w') as json_file:
+            json.dump(full_inputs_dict, json_file, indent=4)
+        not_added = []
+        unimportant_not_added = []
+        print(f'beginning {n} for real')
+
+        inputs_dict = full_inputs_dict[n]
+        search_terms = inputs_dict['search_terms']
+        tz = inputs_dict['tz']
+        location = inputs_dict['location']
+        try:
+            avoid_terms = inputs_dict['avoid_terms']
+        except:
+            avoid_terms = False
+        try:
+            block_terms = inputs_dict['block']
+        except:
+            block_terms = False
+
+        calendar_list = service.calendarList().list(pageToken=None).execute()
+        for cal in calendar_list['items']:
+            if cal['summary'] == f'DAISY{n}':
+                dai_id = cal['id']
+            elif cal['summary'] == f'DAISY{n} (possibly irrelevant)':
+                ir_id = cal['id']
+
+        # Get events, add them to calendar
+        checked_urls = []
+
+            # get events, add to calendars
+        for term in search_terms:
+            events = get_events(term, location,tz)
+            print(events)
+            for event in events:
+                try:
+                    if check_if_exists(service, event, dai_id,tz) or check_if_exists(service, event, ir_id,tz):
+                        continue
+                    print('checked if exists')
+                    #event = format_event(event, tz)
+                    #print('formatted')
+                    [relevant,show] = check_relevance(event, search_terms, avoid_terms,block_terms)
+                    print('relevanced')
+                    # add to calendar or irrelevant event list
+                    if not show:
+                        print(f'not showing {event["summary"]}')
+                        continue
+                    if relevant:
+                        useid = dai_id
+                    else:
+                        useid = ir_id
+                    try:
+                        service.events().insert(calendarId=useid, body=event).execute()
+                    except: # something went wrong adding to calendar
+                        print("something went wrong adding "+event['summary']+" to calendar")
+                        eventstr = str(event['summary'])+"\n"+str(event['description'])
+                        if relevant:
+                            not_added.append(eventstr)
+                        else:
+                            unimportant_not_added.append(eventstr)
+
+                except Exception as exefor:
+                    print("-"*50)
+                    print("-"*50)
+                    print("-"*50)
+                    try:
+                        print("problem formatting \n\n"+str(event)+"\n\n or something, hard to be sure.")
+                        print(exefor)
+                        print(traceback.format_exc())
+                    except:
+                        pass
+                    print("-"*50)
+                    print("-"*50)
+                    print("-"*50)
+
+        # add notes to calendar as 24-h events
+        base_event_dict = {
+            'end':{'dateTime':datetime.today().replace(hour=0,minute=0,second=0,microsecond=0)+timedelta(hours=24),'timeZone':tz},
+            'start':{'dateTime':datetime.today().replace(hour=0,minute=0,second=0,microsecond=0),'timeZone':tz},
+            'summary':'NOTES ON EVENTS NOT ADDED TO CALENDAR'
+            }
+        notes_real_dict = base_event_dict
+        if len(not_added)>0:
+            notes_real_dict['Description'] = str("\n\n".join(not_added))
+            service.events().insert(calendarId=dai_id, body=notes_real_dict).execute()
+        notes_ir_dict = base_event_dict
+        if len(unimportant_not_added)>0:
+            try:
+                notes_ir_dict['Description'] = str("\n\n".join(unimportant_not_added))
+                service.events().insert(calendarId=ir_id, body=notes_ir_dict).execute()
+            except Exception as exe161:
+                print(exe161)
+        try:
+            flag_best(service,tz,dai_id,search_terms,avoid_terms)
+        except:
+            pass
+
+        time.sleep(5 * 60)
